e842a1aa542d4a00fdc317a347e7b23683567177
Biblizar <mathieu.barry31@gmail.com>
2020-06-01 11:46:34 +0200

    update

--- main.py
+++ main.py
@@ -6,23 +6,36 @@ from discord.ext import commands
 #on crée une instance du Bot
 bot = commands.Bot(command_prefix='$')
 
-
 @bot.event
 async def on_ready():
     print("Bot pret")
+
+
+@bot.command()
+@commands.has_role("Modo")
+async def set(ctx):
+    global new_channel_role
+    global new_role_message
+    message = await ctx.send("Ce message est le message de choix de role")
+    new_role_message = message.id
+    print(new_role_message)
+    new_channel_role = message.channel.id
+    print(new_channel_role)
+
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
     emoji = payload.emoji.name
     canal = payload.channel_id
     message = payload.message_id
-    guild = payload.guild_id
     python_role = get(bot.get_guild(payload.guild_id).roles, name="Python")
     php_role = get(bot.get_guild(payload.guild_id).roles, name="Php")
     javascript_role = get(bot.get_guild(payload.guild_id).roles, name="Javascript")
     member = bot.get_guild(payload.guild_id).get_member(payload.user_id)
+    print(f"L'id du channel:{new_channel_role} et l'id du message: {new_role_message}")
     print(message, canal)
-    if canal == "id du channel" and message == "id du message":
+    if canal == new_channel_role and message == new_role_message:
         if emoji == "python":
             await member.add_roles(python_role)
         elif emoji == "php":
@@ -40,13 +53,14 @@ async def on_raw_reaction_remove(payload):
     javascript_role = get(bot.get_guild(payload.guild_id).roles, name="Javascript")
 
     member = bot.get_guild(payload.guild_id).get_member(payload.user_id)
-    if canal == "id du channel" and message == "id du message":
+    if canal == new_channel_role and message == new_role_message:
         if emoji == "python":
             await member.remove_roles(python_role)
         elif emoji == "php":
             await member.remove_roles(php_role)
         elif emoji == "javascript":
             await member.remove_roles(javascript_role)
+
 
 @bot.command()
 async def regles(ctx):
@@ -59,16 +73,16 @@ async def regles(ctx):
 
 @bot.command()
 async def modo(ctx):
-    modo_role = get(bot.get_guild("ID du serveur).roles, name="Modo").mention
+    modo_role = get(bot.get_guild("THE GUILD ID HERE").roles, name="Modo").mention
     embed = discord.Embed(
         title='Les Modérateurs du Discord',
-        description = f'Si tu as un problème n\'hésite pas à contacter un des {modo_role} si dessous',
+        description = f'Si tu as un problème n\'hésite pas à contacter un des {modo_role} ci dessous',
         color=discord.Colour.red()
     )
     embed.set_author(name='BibliBot')
-    embed.add_field(name='Admin', value='@NicolasRz', inline=True)
-    embed.add_field(name='Modérateur1',value='@Djohn', inline=False)
-    embed.add_field(name='Modérateur2',value='@Mikaël', inline=False)
+    embed.add_field(name='Admin du discord', value='@NicolasRz', inline=True)
+    embed.add_field(name='Modérateur ',value='@Djohn', inline=False)
+    embed.add_field(name='  Modérateur ',value='@Mikaël', inline=False)
 
     await ctx.send(embed=embed)
 
@@ -79,7 +93,7 @@ async def bienvenue(ctx, new_member: discord.Member):
     await ctx.send(f"Bienvenue {pseudo} la commande $regles et $help sont la pour toi")
 
 #donner le jeton pour qu'il se connecte
-jeton = "jeton du bot"
+jeton = "ENTER YOUR TOKEN HERE"
 
 print("Lancement du bot")
 
